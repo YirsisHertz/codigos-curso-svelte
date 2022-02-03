220d7f1adf1fbd27a358a0b1a9c260c6bb0ae5a0
YirsisHertz <yirsishertz02@gmail.com>
2022-02-02 21:06:42 -0600

    SvelteKit Update

---
+++ 08-webpack/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Router, { link } from "svelte-spa-router";
+  import { wrap } from "svelte-spa-router/wrap";
+
+  const routes = {
+    "/": wrap({
+      asyncComponent: () => import("./pages/Home.svelte"),
+    }),
+    "/about": wrap({
+      asyncComponent: () => import("./pages/About.svelte"),
+    }),
+    "*": wrap({
+      asyncComponent: () => import("./pages/Error.svelte"),
+    }),
+  };
+</script>
+
+<main>
+  <ul>
+    <li>
+      <a href="/" use:link>Home</a>
+    </li>
+    <li>
+      <a href="/about" use:link>About</a>
+    </li>
+    <li>
+      <a href="/404" use:link>Error</a>
+    </li>
+  </ul>
+  <Router {routes} />
+</main>
---
+++ 08-webpack/src/pages/Error.svelte
@@ -0,0 +1,1 @@
+<h1>Error 404 - Not Page Found</h1>
